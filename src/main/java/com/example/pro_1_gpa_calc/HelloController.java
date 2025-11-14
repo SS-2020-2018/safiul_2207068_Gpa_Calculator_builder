1779709980385c9327d2ec1b628abe2934a41290
SS-2020-2018 <mdsafiulislam@gmail.com>
2025-11-15 01:47:02 +0600

    initial commit '2 scene'

---
+++ src/main/java/com/example/pro_1_gpa_calc/HelloController.java
@@ -0,0 +1,47 @@
+package com.example.pro_1_gpa_calc;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController {
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private ChoiceBox<String> mychoicebox;
+
+    private String[] g = {"A+", "A"};
+
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        mychoicebox.getItems().addAll(g);
+    }
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
