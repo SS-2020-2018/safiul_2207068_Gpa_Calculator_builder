4b9d75a6711a2505eaed6da801f9971c07297295
SS-2020-2018 <mdsafiulislam@gmail.com>
2025-11-15 12:32:57 +0600

    initial commit '2 scene'

--- src/main/java/com/example/pro_1_gpa_calc/HelloController.java
+++ src/main/java/com/example/pro_1_gpa_calc/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.pro_1_gpa_calc;
 import java.io.IOException;
-import java.net.URL;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,26 +7,19 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
 
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    @FXML
-    private ChoiceBox<String> mychoicebox;
-
-    private String[] g = {"A+", "A"};
-
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        mychoicebox.getItems().addAll(g);
-    }
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -36,12 +27,19 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
-
     public void switchToScene2(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    private ChoiceBox<String> mychoicebox;
+    private Label lable;
+
+    public void initialize(URL location, ResourceBundle resources)
+    {
+       // mychoicebox.getItems().addAll("Option 1", "Option 2", "Option 3");
     }
 }
