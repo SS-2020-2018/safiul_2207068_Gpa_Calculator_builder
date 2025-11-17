02c5bf344eb417327944a83b0644e8053a59d10e
SS-2020-2018 <mdsafiulislam@gmail.com>
2025-11-17 15:00:05 +0600

    calculation settings

--- src/main/java/com/example/pro_1_gpa_calc/HelloController.java
+++ src/main/java/com/example/pro_1_gpa_calc/HelloController.java
@@ -1,6 +1,5 @@
 package com.example.pro_1_gpa_calc;
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +7,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -23,34 +21,33 @@ public class HelloController {
     private Scene scene;
     private Parent root;
 
-    public void switchtoscene1(ActionEvent event) throws IOException {
+    public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchtoscene2(ActionEvent event) throws IOException {
+    public void switchToScene2(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public void switchtoscene3(ActionEvent event) throws IOException {
+    public void switchToScene3(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scen3.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    @FXML
+   @FXML
     private VBox courseVBox;
 
     @FXML
-    private void newbutton(ActionEvent event) {
-        HBox crow = new HBox(15);
+    private void handleNewButton(ActionEvent event) {
+        HBox courseRow = new HBox(15);
         TextField name = new TextField();
         name.setPromptText("Course Name");
         name.setPrefWidth(150);
@@ -64,11 +61,13 @@ public class HelloController {
         TextField t2=new TextField();
         t2.setPromptText("Teacher 2 name");
         t2.setPrefWidth(200);
-        ChoiceBox<String>choicebox=new ChoiceBox<>();
-        choicebox.getItems().addAll("A+","A","A-","B+","B","B-","C","F");
-        crow.getChildren().addAll(name, code ,credit,t1,t2,choicebox);
-        courseVBox.getChildren().add(crow);
-        VBox.setMargin(crow, new Insets(10, 0, 10, 0));
+        ChoiceBox<String>choiceBox=new ChoiceBox<>();
+        choiceBox.getItems().addAll("A+","A","A-","B+","B","B-","C","F");
+        courseRow.getChildren().addAll(name, code ,credit,t1,t2,choiceBox);
+        courseVBox.getChildren().add(courseRow);
+        VBox.setMargin(courseRow, new Insets(10, 0, 10, 0));
     }
+
+
 
 }
